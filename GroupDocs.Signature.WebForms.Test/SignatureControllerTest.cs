639a8eec42c11abc6fe3a40118561e394db484b3
pavelteplitsky <pavel.teplitsky@groupdocs.com>
2018-12-14 10:28:54 +0200

    Added tests

---
+++ GroupDocs.Signature.WebForms.Test/SignatureControllerTest.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using Huygens;
+using System;
+
+namespace GroupDocs.Signature.MVC.Test
+{
+    [TestFixture]
+    public class SignatureControllerTest
+    {
+        [Test]
+        public void ViewStatusTest()
+        {
+            string path = AppDomain.CurrentDomain.BaseDirectory + "/../../../src";            
+            using (var server = new DirectServer(path))
+            {
+                var request = new SerialisableRequest
+                {
+                    Method = "GET",
+                    RequestUri = "/signature",
+                    Content = null
+                };
+
+                var result = server.DirectCall(request);
+                Assert.That(result.StatusCode, Is.EqualTo(200));
+            }
+        }        
+    }
+}
