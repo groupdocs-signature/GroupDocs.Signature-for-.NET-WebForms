f311e1587947ca2aa55033118f91fe3a58974153
pavelteplitsky <pavel.teplitsky@aspose.com>
2019-01-30 12:03:18 +0200

    Fixed Node dependencies

--- GroupDocs.Signature.WebForms.Test/SignatureControllerTest.cs
+++ GroupDocs.Signature.WebForms.Test/SignatureControllerTest.cs
@@ -1,6 +1,10 @@
 ﻿using NUnit.Framework;
 using Huygens;
 using System;
+using GroupDocs.Signature.WebForms.Products.Signature.Entity.Web;
+using System.Text;
+using Newtonsoft.Json;
+using System.Collections.Generic;
 
 namespace GroupDocs.Signature.WebForms.Test
 {
@@ -23,6 +27,32 @@ namespace GroupDocs.Signature.WebForms.Test
                 var result = server.DirectCall(request);
                 Assert.That(result.StatusCode, Is.EqualTo(200));
             }
-        }        
+        }
+
+        [Test]
+        public void FileTreeStatusCodeTest()
+        {
+            string path = AppDomain.CurrentDomain.BaseDirectory + "/../../../src";
+            using (var server = new DirectServer(path))
+            {
+
+                SignaturePostedDataEntity requestData = new SignaturePostedDataEntity();
+                requestData.path = "";
+
+                var request = new SerialisableRequest
+                {
+                    Method = "POST",
+                    RequestUri = "/loadfiletree",
+                    Content = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(requestData)),
+                    Headers = new Dictionary<string, string>{
+                        { "Content-Type", "application/json"},
+                        { "Content-Length", JsonConvert.SerializeObject(requestData).Length.ToString()}
+                    }
+                };
+
+                var result = server.DirectCall(request);
+                Assert.That(result.StatusCode, Is.EqualTo(200));
+            }
+        }
     }
 }
